26744b9ef66443f328adedca6fe5187e969113b8
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-07-30 22:44:45 +0530

    Add Java programs for message and number printing

---
+++ Practice/Program69.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Program69
+{
+   static void Display(int iNo)
+    {
+        int iCnt = 0;
+        
+        // filter
+        if(iNo < 0)
+        {
+            System.out.println("Invalid Input");
+            return;
+        }
+
+        for(iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            System.out.println(iCnt);
+        }
+    }
+    public static void main(String arg[])
+    {
+        int iValue = 0;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Fequency: ");
+        iValue = sobj.nextInt();
+        
+        Display(iValue);
+
+    }
+}
