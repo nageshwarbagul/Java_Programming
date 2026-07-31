dd3bb1e0e4034f8b5364f00ce58b23bcc1343c69
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-08-01 00:59:03 +0530

    Java programs for divisibility checking

---
+++ Practice/Program94.java
@@ -0,0 +1,42 @@
+
+// Type 3 
+import java.util.Scanner;
+
+class NumberX
+{
+    public static boolean CheckDivisible(int iNo)
+    {
+         if((iNo % 3 == 0) && (iNo % 5 == 0))
+         {
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+    }
+}
+ class program94
+ {
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue=0;
+        boolean bRet=false;
+
+        System.out.println("Enter Number:");
+         iValue = sobj. nextInt();
+
+         bRet=NumberX.CheckDivisible(iValue);                
+         if (bRet == true)
+        {
+            System.out.println("Number is divisible by 3 ans 5");
+        }
+        else
+        {
+            System.out.println("Number is not divisible by 3 ans 5");
+        }
+    }
+    
+}
