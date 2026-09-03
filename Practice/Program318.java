c1abacccf76bbc688d57da2935aa484aea08edd1
Nageshwar Bagul <nageshwarbagul08@gmail.com>
2026-09-03 23:00:29 +0530

    Add character ASCII and type casting programs

---
+++ Practice/Program318.java
@@ -0,0 +1,10 @@
+class Program318
+{
+    public static void main(String A[])
+    {
+        char ch1 = 'A';
+        
+        System.out.println(ch1);
+        System.out.println((char)(ch1+32));           
+    }
+}
